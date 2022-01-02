3fe46bb597f3a5df73425600982e1a3d9af8f2b1
senjaya <amarpanjis@gmail.com>
2022-01-02 19:29:17 +0700

    update path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,9 +4,10 @@ end
 xrdb ~/.Xresources
 
 set -x PATH $PATH ~/.local/bin
+set -x PATH $PATH $GOPATH/bin
 set -x CPLUS_INCLUDE_PATH $CPLUS_INCLUDE_PATH ~/.local/include
 set -x LIBRARY_PATH $LIBRARY_PATH ~/.local/lib
 set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH ~/.local/lib
 set -x PATH $PATH ~/.dotnet/tools
-set -x GOPATH $GOPATH ~/code/go
 set -x npm_config_prefix $npm_config_prefix ~/.local
+export GOPATH=/home/amay/code/go
